a2ab1da70c7d4f6b8d2c1c42faa8d587d40efb44
daisy-dev <kadanarpa@gmail.com>
2024-10-28 23:44:44 -0500

    feat: first commit

---
+++ src/main/java/co/edu/usco/parcialPW/ParcialIiPwApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usco.parcialPW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialIiPwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialIiPwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usco/parcialPW/ParcialIiPwApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usco.parcialPW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialIiPwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
